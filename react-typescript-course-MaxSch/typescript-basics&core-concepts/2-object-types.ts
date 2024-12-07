e840b5708e3b5c767ff7301c56d3cfcf780b8e44
Berkan Serbes <berkan_serbes@hotmail.com>
2024-12-07 20:31:32 +0300

    Add custom user type definition with properties

--- react-typescript-course-MaxSch/typescript-basics&core-concepts/2-object-types.ts
+++ react-typescript-course-MaxSch/typescript-basics&core-concepts/2-object-types.ts
@@ -16,3 +16,18 @@ const user2: object = {
     age: 25, 
     isAdmin: true
 }
+
+let customUser : {
+    name: string;
+    age: number;
+    isAdmin: boolean;
+    id: number | string;
+};
+
+
+customUser = {
+    name: "Max",
+    age: 32,
+    isAdmin: true,
+    id: 1
+}
